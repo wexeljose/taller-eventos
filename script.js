5f128b51fe16b5d6a6e93620a632d6dbd9edcd71
WexelJose <wexeljose07@gmail.com>
2025-08-10 14:50:43 -0300

    Se agregó el alert del div

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function saludar() {
   alert("¡Hola!");
 }
+
+function saludoDiv() {
+  alert("¡Hola desde el div!");
+}
+
+document
+  .getElementById("container")
+  .addEventListener("click", saludoDiv, mouseEvent);
