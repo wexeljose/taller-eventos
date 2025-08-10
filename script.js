f6a8b68af7adf942bb589fc57ddd6de09a0957c5
WexelJose <wexeljose07@gmail.com>
2025-08-10 15:07:58 -0300

    Se mejoro el manejo de los alerts

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-function saludar() {
-  alert("¡Hola!");
-}
-
-function saludoDiv() {
-  alert("¡Hola desde el div!");
-}
-
 document
-  .getElementById("container")
-  .addEventListener("click", saludoDiv, mouseEvent);
+  .getElementById("saludarBtn")
+  .addEventListener("click", function (event) {
+    event.stopPropagation();
+    alert("¡Hola!");
+  });
+
+document.getElementById("container").addEventListener("click", function () {
+  alert("¡Hola desde el div!");
+});
